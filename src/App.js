d6df7cb5650e421e83e825e351e15c5212243bc1
akmazian <azz@berkeley.edu>
2023-10-08 16:55:41 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import CollegeRanking from './components/CollegeRanking';
-import FindMentor from './components/FindMentor';
-import AboutUs from './components/AboutUs';
-import MyCollegeRanking from './components/MyCollegeRanking';
-import LoginSignup from './components/LoginSignup';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/home/Home'
+import CollegeRanking from './components/collegeRanking/CollegeRanking'
+import FindMentor from './components/findMentor/FindMentor'
+import AboutUs from './components/aboutUs/AboutUs'
+import MyCollegeRanking from './components/user/MyCollegeRanking'
+import LoginSignup from './components/user/LoginSignup'
 import Q1 from './components/questionnaire/Q1'
 import Q2 from './components/questionnaire/Q2'
 import Q3 from './components/questionnaire/Q3'
@@ -20,39 +18,42 @@ import Q10 from './components/questionnaire/Q10'
 import Q11 from './components/questionnaire/Q11'
 
 // import CollegePreferences from './components/CollegePreferences';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Navigation from './components/structure/Navigation.struct'
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/college-ranking" element={<CollegeRanking />} />
-          <Route path="/find-mentor" element={<FindMentor />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/login-signup" element={<LoginSignup />} />
-          <Route path="/my-college-ranking" element={<MyCollegeRanking />} />
-          <Route path="/question1" element={<Q1 />} />
-          <Route path="/question2" element={<Q2 />} />
-          <Route path="/question3" element={<Q3 />} />
-          <Route path="/question4" element={<Q4 />} />
-          <Route path="/question5" element={<Q5 />} />
-          <Route path="/question6" element={<Q6 />} />
-          <Route path="/question7" element={<Q7 />} />
-          <Route path="/question8" element={<Q8 />} />
-          <Route path="/question9" element={<Q9 />} />
-          <Route path="/question10" element={<Q10 />} />
-          <Route path="/question11" element={<Q11 />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route
+                        path='/college-ranking'
+                        element={<CollegeRanking />}
+                    />
+                    <Route path='/find-mentor' element={<FindMentor />} />
+                    <Route path='/about-us' element={<AboutUs />} />
+                    <Route path='/login-signup' element={<LoginSignup />} />
+                    <Route
+                        path='/my-college-ranking'
+                        element={<MyCollegeRanking />}
+                    />
+                    <Route path='/question1' element={<Q1 />} />
+                    <Route path='/question2' element={<Q2 />} />
+                    <Route path='/question3' element={<Q3 />} />
+                    <Route path='/question4' element={<Q4 />} />
+                    <Route path='/question5' element={<Q5 />} />
+                    <Route path='/question6' element={<Q6 />} />
+                    <Route path='/question7' element={<Q7 />} />
+                    <Route path='/question8' element={<Q8 />} />
+                    <Route path='/question9' element={<Q9 />} />
+                    <Route path='/question10' element={<Q10 />} />
+                    <Route path='/question11' element={<Q11 />} />
+                </Route>
+            </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/user/MyCollegeRanking.js
@@ -0,0 +1,40 @@
+import '../findMentor/FindMentor.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function MyCollegeRanking() {
+    return (
+        <div className='mentor-fullscreen-container'>
+            <div className='mentor-center-content'>
+                <img
+                    src='/MyRanking.png'
+                    alt='Mentor'
+                    className='image-blue'
+                    style={{ paddingRight: '100px' }}
+                />
+                <div className='text-body-right'>
+                    <img
+                        src='/MyRankingWords.png'
+                        alt='MyRankingWords'
+                        className='WordCard'
+                        style={{ width: '500px', height: 'auto' }}
+                    />
+                    <Link
+                        to='/question1'
+                        className='subscribe-button'
+                        style={{ marginTop: '20%', width: '350px' }}
+                    >
+                        Get started
+                    </Link>
+                    <img
+                        src='/Try.png'
+                        alt='Try'
+                        style={{ maxWidth: '75%', paddingTop: '2%' }}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyCollegeRanking
