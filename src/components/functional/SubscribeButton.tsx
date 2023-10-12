5938a53b53b24fb5d6040a7a6b144985f90782d9
akmazian <azz@berkeley.edu>
2023-10-11 20:29:14 -0700

    hotfix SubscribeButton

--- src/components/functional/SubscribeButton.tsx
+++ src/components/functional/SubscribeButton.tsx
@@ -3,7 +3,7 @@ export const SubscribeButton = () => (
         href='https://airtable.com/appR1Mc0m0Gvrh4wx/shrgsXpR0QIXTfvHy'
         target='_blank'
         rel='noopener noreferrer'
-        className='bg-highlight text-sm font-extrabold text-white rounded-full px-6 py-3 text-center cursor-pinter no-underline mt-12 inline-block'
+        className='bg-highlight text-sm hover:drop-shadow-standard font-extrabold text-white rounded-full px-6 py-3 text-center cursor-pinter no-underline mt-12 inline-block'
     >
         Join wishlist
     </a>
