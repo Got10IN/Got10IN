d32a7cc295937ca2ed3136882a2a89de23012bcc
akmazian <azz@berkeley.edu>
2023-10-11 20:29:21 -0700

    hotfix LoginSignup

--- src/pages/user/LoginSignup.tsx
+++ src/pages/user/LoginSignup.tsx
@@ -1,56 +1,17 @@
-import axios from 'axios'
-import { useState } from 'react'
+import { SubscribeButton } from '../../components/functional/SubscribeButton'
 import './LoginSignup.css'
 
 function LoginSignup() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleLogin = async (e: any) => {
-        e.preventDefault()
-        try {
-            const response = await axios.post(
-                'https://got10in-backend-cfba39007310.herokuapp.com/login/',
-                { email, password }
-            )
-            if (response.status === 200 && response.data.access_token) {
-                console.log('Login successful!', response.data)
-                localStorage.setItem('isLoggedIn', 'true')
-                alert('Login successful!')
-            } else {
-                console.log('Login failed!')
-                alert('Login failed!')
-            }
-        } catch (error) {
-            console.error('Error during login:', error)
-            alert('Error during login. Please try again.')
-        }
-    }
-
     return (
-        <div className='login-container'>
-            <div className='login-content'>
-                <h1>Login/Signup Page</h1>
-                <div className='login-form'>
-                    <input
-                        type='email'
-                        placeholder='Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type='password'
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button onClick={handleLogin}>Login</button>
-                </div>
-                <p>
-                    Our login and signup features are under development. Please
-                    check back soon!
-                </p>
-            </div>
+        <div className='min-h-[90vh] bg-white w-full flex items-center justify-center flex-col gap'>
+            <p className='text-accent text-center'>
+                Our login and signup features are under development 👉🏼👈🏼
+                <br />
+                Please check back soon!
+                <br />
+                In the meantime,
+            </p>
+            <SubscribeButton />
         </div>
     )
 }
