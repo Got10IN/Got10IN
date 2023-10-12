1c1fe620e5cfecc6035b6dac42afa063deb7c353
akmazian <azz@berkeley.edu>
2023-10-12 13:16:44 -0700

    AboutUs: reorganized imports

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -1,14 +1,14 @@
-import React, { ChangeEvent, useState } from 'react'
 import axios from 'axios'
-import './AboutUs.css'
-import LogoAboutUs from '../../assets/aboutUs/LogoAboutUs.png'
-import Hi from '../../assets/aboutUs/Hi.png'
-import Got10INOutline from '../../assets/aboutUs/GOT10INOutline.png'
+import { ChangeEvent, useState } from 'react'
+
 import Buttons from '../../assets/aboutUs/3buttons.png'
+import ComingSoon from '../../assets/aboutUs/ComingSoon.png'
+import Got10INOutline from '../../assets/aboutUs/GOT10INOutline.png'
+import Hi from '../../assets/aboutUs/Hi.png'
+import LogoAboutUs from '../../assets/aboutUs/LogoAboutUs.png'
 import OurStory from '../../assets/aboutUs/OurStory.png'
 import RoadMap from '../../assets/aboutUs/Roadmap.png'
 import RoadMapTitle from '../../assets/aboutUs/RoadmapTitle.png'
-import ComingSoon from '../../assets/aboutUs/ComingSoon.png'
 import { Button } from '../../components/buttons/Button'
 
 function AboutUs() {
