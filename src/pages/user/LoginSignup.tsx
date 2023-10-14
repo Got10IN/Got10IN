143123eab5d60fcab72214f6cab6b6e20ca39c29
akmazian <azz@berkeley.edu>
2023-10-13 18:50:33 -0700

    Update LoginSignup.tsx

--- src/pages/user/LoginSignup.tsx
+++ src/pages/user/LoginSignup.tsx
@@ -4,7 +4,7 @@ import './LoginSignup.css'
 function LoginSignup() {
     return (
         <div className='min-h-[90vh] bg-white w-full flex items-center justify-center flex-col gap'>
-            <p className='text-accent text-center'>
+            <p className='text-accent text-center mx-8'>
                 Our login and signup features are under development 👉🏼👈🏼
                 <br />
                 Please check back soon!
