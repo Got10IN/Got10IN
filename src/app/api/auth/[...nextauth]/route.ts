2423b30610a2915a63e92aa286a1d043c55b26d2
akmazian <azz@berkeley.edu>
2023-11-05 14:20:57 -0800

    added next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth, { AuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import DiscordProvider from 'next-auth/providers/discord'
+
+const authOptions: AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID ?? '',
+            clientSecret: process.env.GITHUB_SECRET ?? '',
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_ID ?? '',
+            clientSecret: process.env.DISCORD_SECRET ?? '',
+        }),
+    ],
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
