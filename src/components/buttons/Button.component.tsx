cc555afb678c1ae08afdf8fc1f8aeea0f2b55ce0
akmazian <azz@berkeley.edu>
2023-11-09 01:32:08 -0800

    Button: minor style change

--- src/components/buttons/Button.component.tsx
+++ src/components/buttons/Button.component.tsx
@@ -10,8 +10,8 @@ export const Button = ({ children, gray, className, ...args }: IButton) => (
     <button
         className={
             `${
-                gray ? 'bg-white/30' : 'bg-highlight'
-            } text-sm font-extrabold text-white rounded-full px-1/5 py-3 text-center cursor-pinter no-underline block min-w-max ` +
+                gray ? 'bg-white/10 hover:bg-white/30' : 'bg-highlight'
+            } duration-150 text-sm font-extrabold text-white rounded-full px-1/5 py-3 text-center cursor-pinter no-underline block min-w-max ` +
             className
         }
         {...args}
