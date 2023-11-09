9acb0d9ce0527d5209b0a63a8c138e0066a83c1a
akmazian <azz@berkeley.edu>
2023-11-09 01:59:39 -0800

    fixing base route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,14 @@
 export async function GET(request: Request) {
-    return 'Hello from Got10IN'
+    return new Response(JSON.stringify({ message: 'Hello from Got10IN' }), {
+        status: 200,
+    })
 }
 
 export async function POST(request: Request) {
-    return 'Hello from Got10IN (post ver.)'
+    return new Response(
+        JSON.stringify({ message: 'Hello from Got10IN (post ver.)' }),
+        {
+            status: 200,
+        }
+    )
 }
